e37c21a9d345f29b1658e71446c9206e7727064c
aungmyanmar <aungmyanmar532@gmail.com>
2023-10-09 12:11:44 +0700

    added analytic.js

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -103,7 +103,7 @@ const Layout = ({ children, title }: Props) => {
           </Toolbar>
         </AppBar>
         <Box sx={{ width: "100vw", m: 3 }}>{children}</Box>
-        <Script>
+        <Script id="44938">
           {` !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};analytics._writeKey="jRAcp6AoUeCLSqLTKlU9Ht5KQXPQz1K5";;analytics.SNIPPET_VERSION="4.16.1";
             analytics.load("jRAcp6AoUeCLSqLTKlU9Ht5KQXPQz1K5");
             analytics.page();
