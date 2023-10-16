4b8c735f004fc3955b689372a89de41a4a311fc9
aungmyanmar <aungmyanmar532@gmail.com>
2023-10-16 19:04:25 +0700

    add vercel pkg

--- src/store/slices/productSlice.ts
+++ src/store/slices/productSlice.ts
@@ -11,6 +11,8 @@ const initialState: ProductState = {
 export const fetchData = createAsyncThunk("fetchData", async (_, thunkAPI) => {
   try {
     const response = await fetch(`${config.apiBaseUrl}/products`);
+    console.log(`${config.apiBaseUrl}/products`);
+
     const products = await response.json(); //[haha]
     thunkAPI.dispatch(setProduct(products));
   } catch (error) {
